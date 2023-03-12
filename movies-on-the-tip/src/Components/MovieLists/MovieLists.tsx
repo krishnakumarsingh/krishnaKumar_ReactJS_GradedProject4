2d35384509a3b034d78896f8c10fce00c1fb4ce1
krishna Kumar Singh <krishnakumarsingh@users.noreply.github.com>
2023-03-12 23:18:30 +0530

    add

--- movies-on-the-tip/src/Components/MovieLists/MovieLists.tsx
+++ movies-on-the-tip/src/Components/MovieLists/MovieLists.tsx
@@ -19,14 +19,13 @@ const MovieLists = ({ title, moviesList, url, nameData, page }: { title: string,
         <AddToCartContext.Consumer>
             {(addToCartmoviesFn) => (
                 <div className='container'>
-                    {title && <h2>{title}:</h2>}
+                    {title && <h2 className='movie-list-title'>{title}:
+                        {page === "home" && url && <Button onClick={() => window.location.href = url}>View More</Button>}</h2>}
                     <div className='card-list-container'>
                         {
                             moviesList && moviesList.map(item => <MovieList key={item.id as string} addToCartmoviesFn={addToCartmoviesFn} movieData={item as IMovieList} url={url} type={title} nameData={nameData} />)
                         }
                     </div>
-                    {page === "home" && url && <Button onClick={() => window.location.href = url}>View More</Button>}
-                    {/* {page === "home" && <Nav.Link active={true} eventKey="movies-in-theaters" as={Link} to="movies-in-theaters">View More</Nav.Link>} */}
                 </div>
             )}
         </AddToCartContext.Consumer>
